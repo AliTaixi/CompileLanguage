566ee1364dfee834f095fb8b1355ee4e04ccecdc
Taixi <599191669@qq.com>
2021-12-04 12:24:10 +0800

    radix sort algorithm

--- src/test/java/com/poac/csu/sort/RadixSort.java
+++ src/test/java/com/poac/csu/sort/RadixSort.java
@@ -11,95 +11,72 @@ public class RadixSort {
 
 	// from small to big
 	public static int[] intArray = new int[] { 100, 21, 33, 55, 76, 89, 23, 57, 88, 34, 102, 23, 35, 57, 78, 91, 25, 59,
-			90, 36, 22, 77 };	
-	
+			90, 36, 22, 77 };
+
 	public static int getMaxNum() {
 		int max = intArray[0];
-		for (int i = 0; i < intArray.length; i++) {
+		for (int i = 1; i < intArray.length; i++) {
 			if (max < intArray[i]) {
 				max = intArray[i];
 			}
-		}		
+		}
 		return max;
 	}
-	
+
 	public static void radixSort() {
 		int maxNum = getMaxNum();
-		int col = Integer.bitCount(maxNum);
-		int[][] bucket = new int[col][10];
-		
-		int cout = 0;
-		while (cout < intArray.length) {
-			int elem = intArray[cout];
-			while (elem > 0) {
-				int numOfBit = elem % 10;
+		int base = 1; // base number
+		int[] arrCpy = new int[intArray.length];
 
-				elem /= 10;
+		System.arraycopy(intArray, 0, arrCpy, 0, intArray.length);
+		System.out.println("-------------before1----------");
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(arrCpy[i] + " ");
+		}
+		System.out.println();
+
+		int[] tempArray = new int[intArray.length];
+		while (maxNum / base > 0) {
+
+			int[] bucket = new int[10];
+
+			for (int i = 0; i < intArray.length; i++) {
+				bucket[(intArray[i] / base) % 10]++;
 			}
 
-			cout++;
-		}
-		
-		
-		
-	}
-	
-	
-	/**
-	 * unit 位数
-	 * 1
-	 * @param unit
-	 */
-	public void quickSort(int unit) {
-		int[] bucket = new int[10];
-		
-		for(int i = 0; i < intArray.length; i++) {
-			
-		}
-		
-		
-	}
-	
-	
-	public static int getBitCode(int n) {
-		int bitCode = 0;
-		while (n / 10 > 0) {
-			bitCode = n % 10;
-		}
-		return bitCode;
-	}
+			for (int j = 1; j < 10; j++) {
+				bucket[j] += bucket[j - 1];
+			}
 
-	public static int getDecimal(int n) {
-		int dec = 0;
-		while (n / 10 > 0) {
-			dec = n / 10;
-		}
-		return dec;
-	}
+			for (int i = intArray.length - 1; i > 0; i--) {
+				tempArray[bucket[(arrCpy[i] / base) % 10] - 1] = arrCpy[i];
+				bucket[(arrCpy[i] / base) % 10]--;
 
-	public static int getHundredth(int n) {
-		int hund = 0;
-		while (n / 100 > 0) {
-			hund = n / 100;
+			}
+
+			base *= 10;
+			System.arraycopy(tempArray, 0, arrCpy, 0, intArray.length);
 		}
-		return hund;
-	}
-	
-	/**
-	 * 交换数组位置
-	 * @param i
-	 * @param j
-	 */
-	public void swap(int i, int j) {
-		int temp = intArray[i];
-		intArray[i] = intArray[j];
-		intArray[j] = temp;
+		System.arraycopy(arrCpy, 0, intArray, 0, intArray.length);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println(Integer.bitCount(127));
+
+		System.out.println("-------------before----------");
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
+		}
+		System.out.println();
+
+		radixSort();
+
+		System.out.println("-------------after +----------" + "------------------------------");
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
+		}
+		System.out.println();
+
 	}
 
 }
